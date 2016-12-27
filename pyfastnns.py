c0331a74f95e43caedacff2e5bf8f5cb5253c84e
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-12-27 10:18:54 -0800

    first commit

---
+++ pyfastnns.py
@@ -0,0 +1,68 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+u"""
+Fast Nearest Neighbor Search on python using kd-tree
+
+author Atsushi Sakai
+
+usage: see test codes as below
+
+license: MIT
+"""
+import numpy as np
+import scipy.spatial
+
+
+class NNS:
+
+    def __init__(self, data):
+        # store kd-tree
+        self.tree = scipy.spatial.cKDTree(data)
+
+    def search(self, inp):
+
+        if len(inp.shape) >= 2:  # multi input
+            pass
+
+        dist, index = self.tree.query(inp)
+        return index, dist
+
+
+def test():
+    import matplotlib.pyplot as plt
+
+    data2d = np.random.random(10000).reshape(5000, 2)
+    print(data2d)
+
+    #  input2d = np.random.random(2).reshape(2, 1)
+    input2d = np.random.random(2)
+    print(input2d)
+
+    nns1 = NNS(data2d)
+
+    index, dist = nns1.search(input2d)
+    print(index, dist)
+
+    #  data2d =
+
+    plt.plot(data2d[:, 0], data2d[:, 1], ".r")
+    plt.plot(input2d[0], input2d[1], "xk")
+    plt.plot(data2d[index, 0], data2d[index, 1], "xb")
+    plt.show()
+
+    # 3d
+    data3d = np.random.random(15000).reshape(5000, 3)
+    print(data3d)
+
+    #  input2d = np.random.random(2).reshape(2, 1)
+    input3d = np.random.random(3)
+    print(input3d)
+
+    nns2 = NNS(data3d)
+
+    index, dist = nns2.search(input3d)
+    print(index, dist)
+
+
+if __name__ == '__main__':
+    test()
